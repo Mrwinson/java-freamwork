751e9feb8a2048d4a4b8a9fb8d109711945eb6d8
Mrwinson <1660288873@qq.com>
2017-06-09 18:40:20 +0800

    优化调用统计工具类代码

--- cloudm-framework-common/src/main/java/com/cloudm/framework/common/sms/ExectTimeBO.java
+++ cloudm-framework-common/src/main/java/com/cloudm/framework/common/sms/ExectTimeBO.java
@@ -1,85 +1,43 @@
 package com.cloudm.framework.common.sms;
 
-import java.util.Date;
+import lombok.Data;
 
 /**
- * @description:
+ * @description: log日志解析数据存放类
  * @author: Courser
  * @date: 2017/6/8
  * @version: V1.0
  */
+@Data
 public class ExectTimeBO {
-    private String classname;
-    private Date starttime;
+    /**
+     * 方法名
+     */
+    private String className;
+    /**
+     * 调用所耗时间
+     */
     private String time;
-    private Integer maxtime;
-    private Integer mintime;
-    private Integer avgtime;
+    /**
+     * 调用所耗最大时间
+     */
+    private Integer maxTime;
+    /**
+     * 调用所耗最小时间
+     */
+    private Integer minTime;
+    /**
+     * 调用所耗平均时间
+     */
+    private Integer avgTime;
+    /**
+     * 调用次数
+     */
     private Integer calls;
-    private Integer system_id;
+    /**
+     *调用者所用的系统名
+     * 1.公共平台，2.云机械3.0,3.基础资料
+     */
+    private Integer systemId;
 
-    public void setClassname(String classname) {
-        this.classname = classname;
-    }
-
-    public void setStarttime(Date starttime) {
-        this.starttime = starttime;
-    }
-
-    public void setTime(String time) {
-        this.time = time;
-    }
-
-    public void setMaxtime(Integer maxtime) {
-        this.maxtime = maxtime;
-    }
-
-    public void setMintime(Integer mintime) {
-        this.mintime = mintime;
-    }
-
-    public void setAvgtime(Integer avgtime) {
-        this.avgtime = avgtime;
-    }
-
-    public void setCalls(Integer calls) {
-        this.calls = calls;
-    }
-
-    public void setSystem_id(Integer system_id) {
-        this.system_id = system_id;
-    }
-
-    public String getClassname() {
-
-        return classname;
-    }
-
-    public Date getStarttime() {
-        return starttime;
-    }
-
-    public String getTime() {
-        return time;
-    }
-
-    public Integer getMaxtime() {
-        return maxtime;
-    }
-
-    public Integer getMintime() {
-        return mintime;
-    }
-
-    public Integer getAvgtime() {
-        return avgtime;
-    }
-
-    public Integer getCalls() {
-        return calls;
-    }
-
-    public Integer getSystem_id() {
-        return system_id;
-    }
 }
--- cloudm-framework-common/src/main/java/com/cloudm/framework/common/util/StatisticesUtil.java
+++ cloudm-framework-common/src/main/java/com/cloudm/framework/common/util/StatisticesUtil.java
@@ -4,8 +4,6 @@ import com.cloudm.framework.common.sms.ExectTimeBO;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.*;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.*;
 
 /**
@@ -17,7 +15,6 @@ import java.util.*;
 @Slf4j
 public class StatisticesUtil {
 
-    private static String VIEWSTRING = "[";
     private static String APPLYSTRING = "Time==>";
     private static String SEARCHSTRING = "INFO";
     private static String USERTAG = "class";
@@ -27,14 +24,11 @@ public class StatisticesUtil {
      *
      */
     public static List<ExectTimeBO> readFileByLines(String fileName,Integer system_id) {
-        Calendar now = Calendar.getInstance();
-        String a =String.valueOf(now.get(Calendar.YEAR));
         List<ExectTimeBO> boList=new ArrayList<ExectTimeBO>();
         //首先获取本机使用系统
         File file = new File(fileName);
         BufferedReader reader = null;
         try {
-            System.out.println("以行为单位读取文件内容，一次读一整行：");
             InputStreamReader isr = new InputStreamReader(new FileInputStream(file), "UTF-8");
             reader = new BufferedReader(isr);
             String tempString = null;
@@ -42,42 +36,25 @@ public class StatisticesUtil {
             // 一次读入一行，直到读入null为文件结束
             while ((tempString = reader.readLine()) != null) {
                 if (tempString.contains(SEARCHSTRING)) {
-                    ExectTimeBO exectTimeBO = new ExectTimeBO();
                     //获取方法名开始位置
                     int classindex = tempString.indexOf(USERTAG);
                     //字符串分割获取方法名
-                    String classname = tempString.substring(classindex, tempString.indexOf(",", classindex));
-                    //获取开始时间位置
-                    int start_timeindex = tempString.indexOf(VIEWSTRING);
-                    //分割获取时间
-                    String start_time = a+tempString.substring(start_timeindex + 1, tempString.indexOf(",", start_timeindex));
-                    try
-                    {
-                        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd ");
-                        Date starttime = sdf.parse(start_time);
-                        exectTimeBO.setStarttime(starttime);
-                    }
-                    catch (ParseException e)
-                    {
-                        log.error(e.getClass()+e.getMessage()+"出错了");
-                    }
+                    String className = tempString.substring(classindex, tempString.indexOf(",", classindex));
                     //获取调用时间
                     int timeindex = tempString.indexOf(APPLYSTRING);
                     String time = tempString.substring(timeindex + 7, tempString.length() - 2);
-                    // todo set值
-                    exectTimeBO.setTime(time);
-                    exectTimeBO.setClassname(classname);
-                    exectTimeBO.setSystem_id(system_id);
-                    if (classname != null && classname != "") {
+                    // 将相关数据放入对象中
+                    ExectTimeBO exectTimeBO=setExectTimeBO(className,time,system_id);
+                    if (StringUtil.isNotEmpty(className)) {
                         try {
-                            if (map.containsKey(classname)) {
-                                List<ExectTimeBO> timeBOList = map.get(classname);
+                            if (map.containsKey(className)) {
+                                List<ExectTimeBO> timeBOList = map.get(className);
                                 timeBOList.add(exectTimeBO);
-                                map.put(classname, timeBOList);
+                                map.put(className, timeBOList);
                             } else {
                                 List<ExectTimeBO> exectTimeBOList = new ArrayList();
                                 exectTimeBOList.add(exectTimeBO);
-                                map.put(classname, exectTimeBOList);
+                                map.put(className, exectTimeBOList);
                             }
                         } catch (Exception e) {
                             log.error(e.getClass()+e.getMessage()+"出错了{}");
@@ -102,85 +79,76 @@ public class StatisticesUtil {
         return boList;
     }
 
-    //封装对象
-    public static void setStatistic(Date starttime,String classname,int mintime,int maxtime,int avgtime,int calls,int system_id){
+    //将解析的原始log日志数据封装进ExectTimeBO类
+    public static ExectTimeBO setExectTimeBO(String className,String time,Integer systemId){
         ExectTimeBO exectTimeBO = new ExectTimeBO();
-        exectTimeBO.setStarttime(starttime);
-        exectTimeBO.setClassname(classname);
-        exectTimeBO.setMintime(mintime);
-        exectTimeBO.setMaxtime(maxtime);
-        exectTimeBO.setAvgtime(avgtime);
+        exectTimeBO.setClassName(className);
+        exectTimeBO.setTime(time);
+        exectTimeBO.setSystemId(systemId);
+        return  exectTimeBO;
+    }
+
+
+
+    //封装对象
+    public static ExectTimeBO setStatistic(String className,int minTime,int maxTime,int avgTime,int calls,int systemId){
+        ExectTimeBO exectTimeBO = new ExectTimeBO();
+        exectTimeBO.setClassName(className);
+        exectTimeBO.setMinTime(minTime);
+        exectTimeBO.setMaxTime(maxTime);
+        exectTimeBO.setAvgTime(avgTime);
         exectTimeBO.setCalls(calls);
-        exectTimeBO.setSystem_id(system_id);
+        exectTimeBO.setSystemId(systemId);
+        return exectTimeBO;
     }
 
     //获取平均值，最大最小时间和次数
     public static List<ExectTimeBO> forCount(Map<String, List<ExectTimeBO>> logMap) {
         ExectTimeBO exectTimeBO = new ExectTimeBO();
         List<ExectTimeBO> boList=new ArrayList<ExectTimeBO>();
-        Date starttime = null;
-        String classname = null;
+        String className = null;
         int calls = 0;
-        int maxtime = 0;
-        int mintime = 0;
-        int avgtime = 0;
-        int system_id=0;
+        int maxTime = 0;
+        int minTime = 0;
+        int avgTime = 0;
+        int systemId=0;
 
         Iterator<Map.Entry<String, List<ExectTimeBO>>> it = logMap.entrySet().iterator();
         while (it.hasNext()) {
             Map.Entry<String, List<ExectTimeBO>> entry = it.next();
-            //System.out.println("key= " + entry.getKey() + " and value= " + entry.getValue());
             //当只调用一次的方法得出平均，最大，最小时间和调用次数
             List infoList = entry.getValue();
-            //判断次数并给出所耗时间
-            if (infoList.size() == 1) {
-                Iterator iterator = infoList.iterator();
-                while (iterator.hasNext()) {
+            String[] times = new String[infoList.size()];
+            int i = 0;
+            Iterator iterator = infoList.iterator();
+            //使用迭代器将BO类分出来进行算法得出所需数据
+            while (iterator.hasNext()) {
                     ExectTimeBO dd = (ExectTimeBO) iterator.next();
-                    maxtime = Integer.parseInt(dd.getTime());
-                    starttime = dd.getStarttime();
-                    classname = dd.getClassname();
-                    system_id=dd.getSystem_id();
-                }
-                calls = 1;
-                mintime = maxtime;
-                avgtime = maxtime;
-                //System.out.println(starttime + "=============" + maxtime + "-----------" + classname + "---------");
-                //将数据放入对象中
-                setStatistic(starttime,classname,mintime,maxtime,avgtime,calls,system_id);
-            } else {
-                String[] times = new String[infoList.size()];
-                int i = 0;
-                Iterator iterator = infoList.iterator();
-                while (iterator.hasNext()) {
-                    ExectTimeBO dd = (ExectTimeBO) iterator.next();
-                    starttime = dd.getStarttime();
-                    classname = dd.getClassname();
+                    className = dd.getClassName();
                     String str = dd.getTime();
-                    system_id=dd.getSystem_id();
+                    systemId=dd.getSystemId();
                     times[i] = str;
                     i++;
                 }
-                int[] iTime = new int[times.length];
-                for (int j = 0; j < times.length; j++) {
+            int[] iTime = new int[times.length];
+            for (int j = 0; j < times.length; j++) {
                     iTime[j] = Integer.parseInt(times[j]);
-                }
-                calls = iTime.length;
-                maxtime = mintime = iTime[0];
-                int count = 0;
-                for (int j = 0; j < iTime.length; j++) {
-                    if (iTime[j] > maxtime) {
-                        maxtime = iTime[j];
+            }
+            calls = iTime.length;
+            maxTime = minTime = iTime[0];
+            int count = 0;
+            for (int j = 0; j < iTime.length; j++) {
+                    if (iTime[j] > maxTime) {
+                        maxTime = iTime[j];
                     }
-                    if (iTime[j] < mintime) {
-                        mintime = iTime[j];
+                    if (iTime[j] < minTime) {
+                        minTime = iTime[j];
                     }
                     count = count + iTime[j];
-                }
-                avgtime = count / calls;
-                //将数据放入对象中
-                setStatistic(starttime,classname,mintime,maxtime,avgtime,calls,system_id);
             }
+            avgTime = count / calls;
+            //将数据放入对象中
+            setStatistic(className,minTime,maxTime,avgTime,calls,systemId);
             boList.add(exectTimeBO);
         }
         return boList;
