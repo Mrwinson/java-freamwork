04c7be0ff66874ccf73b429269c81f11bf381f2d
lizhaohua <lizhaohua@cloudm.com>
2017-06-29 09:37:06 +0800

    添加初始化方法接口

---
+++ cloudm-framework-common/src/main/java/com/cloudm/framework/common/init/IbizInit.java
@@ -0,0 +1,15 @@
+package com.cloudm.framework.common.init;
+
+/**
+ * @description: 业务需要在框架启动的调用方法
+ * @author: Courser
+ * @date: 2017/6/29
+ * @version: V1.0
+ */
+public interface IbizInit {
+
+    /**
+     * 初始化方法
+     */
+    void init();
+}
